08bb8884dd98e14af69bb6e34431b6f84373fce1
dennis <denya10.02@googlemail.com>
2025-05-05 21:10:38 +0200

    First Commit

---
+++ src/main/java/demoqa/pages/BasicAuth.java
@@ -0,0 +1,13 @@
+package demoqa.pages;
+
+import demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasicAuth extends BasePage {
+
+
+    public BasicAuth(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+}
